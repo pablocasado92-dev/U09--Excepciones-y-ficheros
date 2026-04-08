f1b3051398749744b9fa09b7b88e7dd4c8a4ed29
pcc <pablo.casado92@gmail.com>
2026-04-08 12:12:33 +0200

    Version 3 con metodo recursivo

---
+++ a01/MainRecursivo.java
@@ -0,0 +1,26 @@
+package a01;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class MainRecursivo {
+    static Scanner sc = new Scanner(System.in);
+
+    static Integer leerEntero(){
+        try{
+            System.out.print("Ingrese un número entero: ");
+            return sc.nextInt();
+            
+        }catch(InputMismatchException e){
+            System.out.println("El número introducido tiene que ser de tipo entero");
+            sc.nextLine();
+            return leerEntero();  
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer numero = leerEntero();
+        System.out.println("Número ingresado: " + numero);
+        sc.close();
+    }
+}
