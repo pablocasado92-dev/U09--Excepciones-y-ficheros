60bb2257f14e46d774689971510c9232c0d5f582
pcc <pablo.casado92@gmail.com>
2026-04-08 10:26:38 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# UD09 - Programación en Java
+
+Repositorio para las actividades de la Unidad Didáctica 09.
+
+## 📁 Estructura
+
+Las actividades están organizadas en carpetas:
+
+- `A01` - Actividad con try-catch, con 3 versiones distintas
+
+*(Cada carpeta contiene sus archivos `.java` correspondientes)*
---
+++ a01/Main.java
@@ -0,0 +1,29 @@
+package a01;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    static Integer leerEntero(){
+        Integer n = null;
+        do{
+            try{
+                System.out.print("Ingrese un número entero: ");
+                n = sc.nextInt();
+                return n;
+            }catch(InputMismatchException e){
+                System.out.println("El número introducido tiene que ser de tipo entero");
+                sc.nextLine();  
+            }
+        } while(n == null);
+        return n;
+    }
+
+    public static void main(String[] args) {
+        Integer numero = leerEntero();
+        System.out.println("Número ingresado: " + numero);
+        sc.close();
+    }
+}
